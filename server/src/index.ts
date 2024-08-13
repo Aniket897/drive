b5668a24e5b6a3061edd1a5cac7dff5baa33ed00
Aniket Kangane <aniketbkangane9637@gmail.com>
2024-08-13 22:42:27 +0530

    template created

---
+++ server/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
+
+//listning
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
