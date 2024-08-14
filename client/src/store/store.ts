f9c5401cbe8b880c00ca8246a6fff31d800945c1
Aniket Kangane <aniketbkangane9637@gmail.com>
2024-08-14 23:59:47 +0530

    persistor added

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,14 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
 import documentSlice from "./slices/documentSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    auth: authSlice,
-    document: documentSlice,
-  },
+const rootPersistConfig = {
+  key: "root",
+  storage,
+  keyPrefix: "redux-",
+  //   whitelist: [],
+  blacklist: ["documents"],
+};
+
+const rootReducers = combineReducers({
+  auth: authSlice,
+  document: documentSlice,
 });
 
+const store = configureStore({
+  reducer: persistReducer(rootPersistConfig, rootReducers),
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
